5689cc3aa7b5006ff8553850142e57fd22d3cad9
Guillaume <H.guillaume@gmail.com>
2023-09-28 23:38:19 +0200

    initial commit

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+    experimental: {
+      urlImports: ['https://framer.com/m/', 'https://framerusercontent.com/modules/'],
+    },
+    output: 'export'
+  }
