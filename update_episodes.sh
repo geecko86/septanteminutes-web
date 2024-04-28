ab880906e91ec2ce6bb4ff9e2f1e9e2df8c557a6
Guillaume Hachez <H.guillaume@gmail.com>
2024-04-28 17:07:19 +0200

    perspective and shell script

---
+++ update_episodes.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Function to fetch content from URL and store it in a variable
+fetch_content() {
+    content=$(curl -sS "$1")
+    echo "$content"
+}
+
+# Main script
+retry_count=0
+max_retries=3
+
+while [ $retry_count -lt $max_retries ]; do
+    echo "Fetching content from URL..."
+    fetched_content=$(fetch_content "https://europe-west1-septanteminutes.cloudfunctions.net/getEpisodesFromRSS")
+
+    # Check if fetched content contains "error" (case insensitive)
+    if [[ $fetched_content =~ [Ee][Rr][Rr][Oo][Rr] ]]; then
+        echo "Fetched content contains error, retrying..."
+        ((retry_count++))
+    else
+        echo "Content fetched successfully."
+        break
+    fi
+done
+
+if [ $retry_count -eq $max_retries ]; then
+    echo "Maximum retries reached. Failed to fetch content."
+    exit -1
+fi
+
+npx -p node-firestore-import-export firestore-export -a septanteminutes-a0cde5efbc25.json -b public/js/data.json
+sed -i '' -E 's/^.{19}//; s/.$//; s/{}},"/{}},\n"/g' public/js/data.json
+exit
+
+# sed -i '' -E 's/^.{19}//; s/.$//' public/js/data.json
