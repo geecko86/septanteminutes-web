880debc2be2651607870152c09902185509b97a2
Guillaume Hachez <H.guillaume@gmail.com>
2024-06-04 19:16:08 +0200

    feat(Offline): attempt to create offline fallback page

---
+++ src/pages/_offline.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import { Spotlight } from "../components/Spotlight";
+import styles from "./404.module.css"
+import Link from "next/link";
+
+export default function CustomFallback() {
+    const router = useRouter();
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        const main = document.querySelector('main');
+        const context = document.querySelector('canvas')?.getContext('2d');
+        if (main && context) {
+            import("../utils/tv").then((module) => {
+                const toggleTV = module.default;
+                toggleTV(visible, main!!, context!!, styles.on, styles.off);
+            });
+        }
+    }, [visible]);
+
+    const leaving = () => {
+        setVisible(false);
+        setTimeout(() => {
+            router.push("/")
+        }, 800);
+    };
+
+    return (
+        <div className={styles.container} style={{
+            opacity: visible ? 1 : 0,
+            transition: "opacity 0.15s ease-in-out 0.6s"
+        }}>
+            <Head>
+                <title>404 - Septante Minutes Avec Personne</title>
+            </Head>
+            <main className={["scanlines", styles.scanlines].join(" ")}>
+                <div className={["screen", styles.screen].join(" ")}>
+                    <canvas id="canvas" className={["picture", styles.picture].join(" ")}></canvas>
+                    <div className={["overlay", styles.overlay].join(" ")} />
+                    <div className={styles.textContainer}>
+                        <h1>
+                            Offline
+                        </h1>
+                        <p style={{textAlign: "justify"}}>
+                            {`Hors-ligne (adj.) : Désigne un appareil, un système ou une personne qui n'est pas connectée à Internet. Par exemple : vous.`}
+                            <br/><br/>
+                            <u onClick={leaving}><Link href="mailto:contact@septanteminutes.be">contact@SeptanteMinutes.be</Link></u>
+                            <br />
+                        </p>
+                    </div>
+                    <Spotlight className={styles.spotlight} fill="white" />
+                </div>
+            </main>
+        </div>
+    )
+}  
